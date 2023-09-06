0021ed8418aeb9f13c5a77f74dfdbbfe0b1c3844
Pedro <paraoz22@gmail.com>
2023-09-06 20:09:17 -0300

    Updated swagger

--- src/controllers/info.ts
+++ src/controllers/info.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
 const getInfo = async (req: Request, res: Response, next: NextFunction) => {
+  /* 
+  #swagger.description = 'Healthcheck endpoint'
+  */
   try {
     return res.status(200).json({ status: "OK" });
   } catch (error) {
