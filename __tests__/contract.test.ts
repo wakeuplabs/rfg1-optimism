7bf21edfd530916d532b0b26abf6758cf2777a26
juandahl <juandahl20@gmail.com>
2023-12-04 17:13:54 -0300

    fix: install dependencies

--- __tests__/contract.test.ts
+++ __tests__/contract.test.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
 /* eslint-disable @typescript-eslint/no-var-requires */
 import 'jest';
 const request = require("supertest");
